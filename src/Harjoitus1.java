4e2b7e44d8c3c51b278f1b56abd998ee0404797c
Sanna Toivanen <sanna.toivanen@student.hamk.fi>
2023-10-17 12:07:32 +0300

    Initial commit

---
+++ src/Harjoitus1.java
@@ -0,0 +1,29 @@
+
+public class Harjoitus1 {
+
+	public static void main(String[] args) {
+	
+		String otsikko = "Hei olen Tulostin-ohjelma";
+		
+		System.out.println(otsikko);
+		
+		String tekija = "Sanna";
+		
+		System.out.println("Ohjelman tekijä: " + tekija);
+		
+		double luku1 = 5;
+		double luku2 = 2;
+		
+		System.out.println("Luku1-muuttujan arvo on " + luku1);
+		System.out.println("Luku2-muuttujan arvo on " + luku2);
+		
+		double tulo;
+		
+		tulo = (luku1 * luku2);
+		
+		System.out.println(luku1 + " * " + luku2 + " = " + tulo );
+		
+		
+	}
+
+}
